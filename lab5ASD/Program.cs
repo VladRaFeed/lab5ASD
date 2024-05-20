ecf3d941171bd7da5b2c1517f31a770e077c0aab
Vinga <jack22toplolkek@gmail.com>
2024-05-20 23:51:24 +0300

    Добавьте файлы проекта.

---
+++ lab5ASD/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace lab5asd
+{
+    class TreeNode
+    {
+        public int key;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode parent;
+
+        public TreeNode()
+        {
+            key = 0;
+            left = null;
+            right = null;
+            parent = null;
+        }
+
+        public TreeNode(int key)
+        {
+            this.key = key;
+            left = null;
+            right = null;
+            parent = null;
+        }
+    }
+
+    class Program
+    {
+        public void IndorderTreeWalk(int x)
+        {
+            if(x != null)
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
